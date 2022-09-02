1199fdd97c0b37a5ea0d9b8df73684829dd0ec26
Dennis Nguyen <dnsnguy08@gmail.com>
2022-09-01 22:02:15 -0700

    remove db security leak

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect('mongodb+srv://dennis:4pbPg9c0BgOGwtz6@cluster0.hc8bssf.mongodb.net/socialNetworkApp?retryWrites=true&w=majority')
-.then(() => {
-  console.log('success!'); 
-})
-.catch(err => console.log(err));
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
